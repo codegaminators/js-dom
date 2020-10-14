9d651174343a6de3a33cd9d343e1996219f45426
Peter Akaliro <pin4eva@hotmail.com>
2020-10-14 15:05:53 +0100

    first commit

--- app.js
+++ app.js
@@ -1,64 +1,31 @@
-const users = [
-  {
-    id: "6caaeede-a463-4ca4-812b-27cdd408b2e5",
-    first_name: "Vita",
-    last_name: "Klageman",
-    email: "vklageman1i@digg.com",
-    gender: "Female",
-    password: "password1",
-    username: "user1",
-  },
-  {
-    id: "bec7124d-6822-414d-ae52-491ec9e8ecc6",
-    first_name: "Keriann",
-    last_name: "Di Francecshi",
-    email: "kdifrancecshi1j@edublogs.org",
-    gender: "Female",
-    password: "password2",
-    username: "user2",
-  },
-  {
-    id: "fb2a1d52-d3e6-442c-b808-533ce2f50ecd",
-    first_name: "Ramsay",
-    last_name: "Chappell",
-    email: "rchappell1k@icio.us",
-    gender: "Male",
-    password: "password3",
-    username: "user3",
-  },
-];
-const form = document.querySelector("#myform");
+const mobileWrapper = document.querySelector(".mobile-wrapper");
 const toggler = document.querySelector(".toggler");
-const h3 = document.createElement("h3");
-const ul = document.createElement("ul");
-const li = document.createElement("li");
-const bio = document.querySelector("#bio");
+const form = document.querySelector("#form");
+let checkValue = false;
+const users = [];
+
+const check = document.getElementsByName("check")[0];
+check.addEventListener("change", (e) => {
+  checkValue = !checkValue;
+  console.log(checkValue);
+});
 
 toggler.addEventListener("click", () => {
-  const mobileNav = document.querySelector(".mobile-nav");
-  mobileNav.classList.toggle("d-none");
+  mobileWrapper.classList.toggle("show");
 });
 
 form.addEventListener("submit", (event) => {
-  const username = document.getElementsByName("username")[0];
-  const password = document.getElementsByName("password")[0];
   event.preventDefault();
-  const data = users.find(
-    (user) =>
-      user.username === username.value && user.password === password.value
-  );
-  if (data) {
-    alert(`Welcome ${data.first_name} ${data.last_name}`);
-    handleSuccess(data);
-  } else alert("Username or password is not correct");
+  let name = document.getElementsByName("name")[0].value;
+  let username = document.getElementsByName("username")[0].value;
+  let email = document.getElementsByName("email")[0].value;
+  let password = document.getElementsByName("password")[0].value;
+  let password2 = document.getElementsByName("password2")[0].value;
+
+  if (password !== password2) {
+    return alert("Passwords don't match");
+  }
+  users.push({ name, username, email, password });
+  console.log(users);
+  alert("SUCCESS !");
 });
-
-function handleSuccess(info) {
-  h3.textContent = `Welcome ${info.first_name}`;
-
-  li.textContent = `Name: ${info.first_name} ${info.last_name}`;
-
-  bio.appendChild(h3);
-  bio.appendChild(li);
-  form.className = "d-none";
-}
